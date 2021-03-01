a55e5898e8f417775c077bb943d24b88663dfcc8
Áine Thompson <ainegthompson@gmail.com>
2021-03-01 04:07:31 +0000

    Completed get_ratings

--- ratings.py
+++ ratings.py
@@ -1,2 +1,17 @@
 """Restaurant rating lister."""
 
+
+def get_ratings(filename):
+
+    ratings = {}
+
+    for line in open(filename):
+        data = line.rstrip().split(':')
+
+        ratings[data[0]] = data[1]
+
+
+    for item in sorted(ratings.items()):
+        print(f"{item[0]} is rated at {item[1]}")
+
+get_ratings('scores.txt')
